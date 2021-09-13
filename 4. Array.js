423a4c4f29c0e35270ad5d4e659749a7b04d254e
Jaekomplett <wanzekim@gmail.com>
2021-09-14 01:10:05 +0900

    ---

--- 4. Array.js
+++ 4. Array.js
@@ -5,6 +5,8 @@ let arr = [1, 2, 3, 4];
 console.log(arr); // [1, 2, 3, 4]
 console.log(arr[0]); // 1
 
+
+
 // 1. concat() 
 //! No Mutation
 
@@ -13,6 +15,8 @@ let arrTwo = ['pineapple', 'kiwi'];
 
 console.log(arr.concat(arrTwo));
 //[ 'apple', 'pear', 'orange', 'banana', 'pineapple', 'kiwi' ]
+
+
 
 
 // 2. 배열에 항목 추가, 제거하기
@@ -37,6 +41,8 @@ console.log(arr);// [ 'kiwi', 'apple', 'pear', 'orange', 'banana' ]
 console.log(arr.shift()); // 'kiwi' 삭제
 console.log(arr);// [ 'apple', 'pear', 'orange', 'banana' ]
 
+
+
 // 3. splice(): 요소를 삭제하거나 교체
 // arr.splice(시작 인덱스, 삭제할 요소 수, 추가할 요소)
 //! mutates
@@ -50,6 +56,8 @@ console.log(arr.splice(1, 0, 'mango')); // []
 console.log(arr); // [ 'apple', 'mango', 'pear', 'kiwi', 'banana' ]
 
 
+
+
 // 4. slice(): 요소를 인덱스 기준으로 잘라낸다.
 // arr.slice(시작 인덱스 이상부터, 종료 인덱스 미만까지)
 //! NO Mutation
@@ -57,6 +65,8 @@ let arr = [3, 5, 2, 7, 8, 10, 1];
 
 console.log(arr.slice(0,4)); // [ 3, 5, 2, 7 ]
 console.log(arr.slice(2,4)); // [ 2, 7 ]
+
+
 
 
 // 5. fill(): 해당 값으로 배열을 채운다. 
@@ -70,6 +80,9 @@ console.log(arr.fill(100, 3, 5)); // [ 0, 0, 0, 100, 100, 0 ]
 console.log(arr.fill(99, 2, 5)); // [ 0, 0, 99, 99, 99, 0 ]
 console.log(arr.fill(99, 2, 6)); // [ 0, 0, 99, 99, 99, 99 ]
 
+
+
+
 // 6. filter(): 조건을 만족하는 항목들만 배열로 반환한다.
 //! No Mutation
 let score = [10, 30, 55, 80, 95, 100, 70];
@@ -80,3 +93,31 @@ console.log(score.filter(x => x < 55)); // [ 10, 30 ]
 
 console.log(words.filter(x => x.length > 4)); // [ 'hardy', 'dicaprio' ]
 console.log(words.filter(x => x.length > 3)); // [ 'hardy', 'dicaprio', 'mary', 'jane' ]
+
+
+// 7. flat() 
+//! No Mutation
+let matrix = [1, 2, 3, [1, 2, 3, [10, 20]]];
+
+console.log(matrix.flat());    // [ 1, 2, 3, 1, 2, 3, [ 10, 20 ] ]
+console.log(matrix.flat(2));   // [ 1, 2, 3, 1, 2, 3, 10, 20 ]
+
+
+
+
+// 8. includes(): 배열이 특정 항목을 포함하고 있는지 확인한다.
+//! No Mutation
+let fruits = ['apple', 'banana', 'pear', 'cherry'];
+
+fruits.includes('apple'); // true
+fruits.includes('grape'); // false
+
+
+
+
+// 9. join(): 배열의 항목들을 특정 값으로 이어 붙인다.
+//! No Mutation
+let arr = [1, 2, 3, 4, 5];
+
+console.log(arr.join('!')); // '1!2!3!4!5'
+console.log(arr.join('--a')); // '1--a2--a3--a4--a5'
